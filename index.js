8b72a2b76a29541917a55525ed12b0aafea15cfd
CAIO RODRIGUES <951551430@adm.unifor.br@DTEC65070525.unifor.br>
2025-04-28 16:48:27 -0300

    modificando as rotas

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express"); //importa o mÃ³dulo express neste arquivo
 const app = express(); //iniciando o express
 
+let clientes = [];
 //criando a rota inicial
 app.get("/", function(req,res){
     res.send("<h1>Bem vindo ao meu site!</h1>");
 })
 
 //rota do cadastro de produtos
-app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de Produtos!</h1>");
+app.get("/clientes", function(req,res){
+    res.write("<h1>Lista de Clientes!</h1>");
+    for(let i = 0; i < clientes.length;i++){
+        res.write("<li>"+clientes[i]+" ----- id: "+i+"</li>");
+    }
+    
 })
 
 //rota com parametro 
 app.get("/consulta/:parametro", function(req,res){
     //req --> dados enviados pelo cliente
     //res --> resposta enviada pelo servidor de volta ao cliente
-    res.send("retorno consulta:" + req.params.parametro);
+    let idcli = req.params.parametro;
+    res.send("<h1>Cliente: " + clientes[idcli]+"</h1>");
 })
 
 //rota com parametro opcional
@@ -23,14 +29,15 @@ app.get("/cadastro/:nome", function(req,res){
     //req --> dados enviados pelo cliente
     var nome = req.params.nome;
     if (nome){
-        res.send("<h1>produto " + nome + " criado!</h1>");
+        clientes.push(nome)
+        res.send("<h1>Cliente " + nome + " cadastrado!</h1>");
     }else{
-        res.send("produto criado!");
+        res.send("cliente não cadastrado, não existe parametro!");
     }
     
 })
 
-app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicaÃ§Ã£o na porta 4000
+app.listen(process.env.PORT ?? 4000,function(erro){  // cria a aplicaÃ§Ã£o na porta 4000
     if (erro){
         console.log("Erro ao Iniciar.");
     }else{
